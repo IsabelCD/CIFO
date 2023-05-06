317532b42d284dfe3f61a1db139797c928b366a9
IsabelCD <isabelmcoutodias@gmail.com>
2023-05-06 13:58:11 +0100

    Introdução do código
    Posto o código das aulas como ponto de partida para o nosso projeto.

---
+++ charles/crossover.py
@@ -0,0 +1,62 @@
+from random import randint
+
+
+def single_point_co(p1, p2):
+    """Implementation of single point crossover.
+
+    Args:
+        p1 (Individual): First parent for crossover.
+        p2 (Individual): Second parent for crossover.
+
+    Returns:
+        Individuals: Two offspring, resulting from the crossover.
+    """
+    co_point = randint(1, len(p1)-2)
+
+    offspring1 = p1[:co_point] + p2[co_point:]
+    offspring2 = p2[:co_point] + p1[co_point:]
+
+    return offspring1, offspring2
+
+
+def cycle_xo(p1, p2):
+    """Implementation of cycle crossover.
+
+    Args:
+        p1 (Individual): First parent for crossover.
+        p2 (Individual): Second parent for crossover.
+
+    Returns:
+        Individuals: Two offspring, resulting from the crossover.
+    """
+    # offspring placeholders
+    offspring1 = [None] * len(p1)
+    offspring2 = [None] * len(p1)
+
+    while None in offspring1:
+        index = offspring1.index(None)
+        val1 = p1[index]
+        val2 = p2[index]
+
+        # copy the cycle elements
+        while val1 != val2:
+            offspring1[index] = p1[index]
+            offspring2[index] = p2[index]
+            val2 = p2[index]
+            index = p1.index(val2)
+
+        # copy the rest
+        for element in offspring1:
+            if element is None:
+                index = offspring1.index(None)
+                if offspring1[index] is None:
+                    offspring1[index] = p2[index]
+                    offspring2[index] = p1[index]
+
+    return offspring1, offspring2
+
+
+if __name__ == '__main__':
+    p1, p2 = [2, 7, 4, 3, 1, 5, 6, 9, 8], [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    o1, o2 = cycle_xo(p1, p2)
+    print(o1, o2)
