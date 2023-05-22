635d3de46801b55095c98945d9e75f15c42315c3
joanapardelha <joanapms01@gmail.com>
2023-05-22 12:48:58 +0100

    update fitness

--- timetabling.py
+++ timetabling.py
@@ -1,35 +1,29 @@
 from charles.charles import Population, Individual
-from data.tsp_data import distance_matrix
-from random import choices
-from copy import deepcopy
 from charles.crossover import cycle_xo
-from charles.mutation import swap_mutation
-from charles.selection import tournament_sel, fps
-from data_import import *
+from charles.mutation import binary_mutation
+from charles.selection import tournament_sel
+from pop_created import *
+from pop_creation import *
+
 
 def get_fitness(self):
-    # Create a dictionary with days as keys and exam codes as values
+    # Create a dictionary with days as keys and exams as values
     #using a dictionary comprehension is more efficient than with an if statement  on a list
-    exams_by_day = {}
-    for exam in self.representation:
-        day = exam[0]["day"]
-        code = exam[1]
-        exams_by_day.setdefault(day, []).append(code)
-
-    # Convert the dictionary values to a list
-    exams_by_day = list(exams_by_day.values())
 
     # Create an empty list to hold nº students with multiple exams in each day
     student_counts = []
+    exam_by_day = {}
 
     # Iterate over each sub-list in the result list
-    for exam_list in exams_by_day:
-        
-        # Filter the dataframe to only include rows with the current day's exams
-        day_en = df_en[df_en['exam'].isin(exam_list)]
-        
+    for day, day_keys in hours_keys.items():
+        exam_by_day[day] = []
+        for key_day in day_keys:
+            exam_by_day[day] = exam_by_day[day] + (self.representation[key_day])
+
+    for day, exams in exam_by_day.items():
+
         # Get the list of students who took the exams on the current day
-        students = list(day_en['student'])
+        students = check_students(exams, [], list_students= True)
         
         # Calculate the difference between the number of students who took the exams and the number of unique students
         diff = len(students) - len(set(students))
@@ -45,17 +39,20 @@ def get_fitness(self):
 
 # Monkey patching
 Individual.get_fitness = get_fitness
-Individual.get_neighbours = get_neighbours
 
 
-pop = Population(
-    size=50,
-    sol_size=len(distance_matrix[0]),
-    valid_set=[i for i in range(len(distance_matrix[0]))],
-    replacement=False,
-    optim="min")
 
-pop.evolve(gens=500, select=tournament_sel, mutate=swap_mutation, crossover=cycle_xo,
+
+
+# Step 2: Iterate over your list of lists and create Individual instances
+for i, inner_list in enumerate(population):
+    individual = Individual(representation=inner_list)
+    print(i)
+
+# Step 3: Assign the created Individual instances to the individuals list of the Population instance
+pop= Population(size =30, replacement=False, optim= 'min')
+
+pop.evolve(gens=50, select=tournament_sel, mutate=binary_mutation, crossover=cycle_xo,
            mut_prob=0.05, xo_prob=0.9, elitism=True)
 
 
